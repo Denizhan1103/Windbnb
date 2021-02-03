38f7106cd875f58e5b0d364cfff6e2e41648015f
Denizhan Onur <denizhanonr2000@gmail.com>
2021-02-03 19:41:37 +0300

    add require for image list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -16,42 +16,48 @@ export default {
           construct: 'Entire apartments. 2 beds',
           star: '4.40',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel1.jpg')
         },
         {
           name: 'Cozy, peaceful and private room with...',
           construct: 'Private room',
           star: '4.25',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel2.jpg')
         },
         {
           name: 'Modern house in a remote area',
           construct: 'Entire house',
           star: '4.96',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel3.jpg')
         },
         {
           name: 'Stylist room in design district',
           construct: 'Entire apartments. 2 beds',
           star: '4.85',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel4.jpg')
         },
         {
           name: 'Modern apartment close to nature',
           construct: 'Private room',
           star: '4.54',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel5.jpg')
         },
         {
           name: 'House in a remote area',
           construct: 'Entire house',
           star: '4.64',
           properties: ['super', 'normal'],
-          type: 'SUPER HOST'
+          type: 'SUPER HOST',
+          image: require('../assets/hotel6.jpg')
         }
       ]
     }
