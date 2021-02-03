dd49de4ae5f2924ea91245f6350f1a908973a1b2
Denizhan Onur <denizhanonr2000@gmail.com>
2021-02-03 19:28:41 +0300

    add product item

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="product-item"></div>
+  <div class="product-item">
+    <div class="product-item__image"></div>
+    <div class="product__item-data">
+      <div class="data__up">
+        <div class="data__up-type">{{detail.type}}</div>
+        <div class="data__up-construct">{{detail.construct}}</div>
+        <div class="data__up-star">{{detail.star}}</div>
+      </div>
+      <p class="data__down">{{detail.name}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  prop: {
+    detail: Object
+  }
 }
 </script>
 
