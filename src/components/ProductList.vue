67a33d52daea886abcbbad01265d067fadb8164b
Denizhan Onur <denizhanonr2000@gmail.com>
2021-02-03 19:16:41 +0300

    added data

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,7 +4,54 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: [ 
+        {
+          name: 'Stylist apartment in center of the city',
+          construct: 'Entire apartments. 2 beds',
+          star: '4.40',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        },
+        {
+          name: 'Cozy, peaceful and private room with...',
+          construct: 'Private room',
+          star: '4.25',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        },
+        {
+          name: 'Modern house in a remote area',
+          construct: 'Entire house',
+          star: '4.96',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        },
+        {
+          name: 'Stylist room in design district',
+          construct: 'Entire apartments. 2 beds',
+          star: '4.85',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        },
+        {
+          name: 'Modern apartment close to nature',
+          construct: 'Private room',
+          star: '4.54',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        },
+        {
+          name: 'House in a remote area',
+          construct: 'Entire house',
+          star: '4.64',
+          properties: ['super', 'normal'],
+          type: 'SUPER HOST'
+        }
+      ]
+    }
+  }
 }
 </script>
 
